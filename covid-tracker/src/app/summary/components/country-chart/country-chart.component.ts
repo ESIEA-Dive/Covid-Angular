6720f9ad09ff83a9319cbfd95e53f21535d3cfdb
unknown <dive@et.esiea.fr>
2022-12-14 22:59:08 +0100

    Chartjs Static Data

--- covid-tracker/src/app/summary/components/country-chart/country-chart.component.ts
+++ covid-tracker/src/app/summary/components/country-chart/country-chart.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-country-chart',
   templateUrl: './country-chart.component.html',
   styleUrls: ['./country-chart.component.css']
 })
-export class CountryChartComponent {
+export class CountryChartComponent implements OnInit {
+
+  barChartData: ChartDataSets[] = [
+    {
+      data: [64, 80 ,98]
+    }
+  ];
+  barChartLabels: Label[] = ['Usa', 'Brazil', 'fuk'];
+  barChartOptions: ChartOptions = {
+    responsive: true
+  }
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
