830596550de383b5fe11507d5256a5b8e5db3ed3
unknown <dive@et.esiea.fr>
2022-12-14 15:20:52 +0100

    Top 5 countries

--- covid-tracker/src/app/summary/components/summary-cards/summary-cards.component.ts
+++ covid-tracker/src/app/summary/components/summary-cards/summary-cards.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-summary-cards',
   templateUrl: './summary-cards.component.html',
   styleUrls: ['./summary-cards.component.css']
 })
+
 export class SummaryCardsComponent implements OnInit, OnChanges {
   
   @Input() covidData: any;
