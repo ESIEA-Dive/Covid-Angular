be79e43a1af17ac983f962cc65731f24c02dd227
unknown <dive@et.esiea.fr>
2022-12-14 17:14:28 +0100

    Search Display in By Country

--- covid-tracker/src/app/country/components/data-table/data-table.component.ts
+++ covid-tracker/src/app/country/components/data-table/data-table.component.ts
@@ -1,21 +1,39 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SearchService } from 'src/app/shared/services/search.service';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent  implements OnInit, OnChanges {
+export class DataTableComponent  implements OnInit, OnChanges, OnDestroy {
   
   @Input() covidData: any;
   countries: any[] = [];
 
-  constructor() {}
+  // To prevent memory leak 
+  subscription = new Subscription();
+
+  constructor(
+    private searchService: SearchService
+    ) {}
 
   ngOnInit(): void { 
+    this.subscription = this.searchService.getCountries()
+      .subscribe(
+        (data: any) => {
+          this.countries = data;
+        }
+      );
   }
 
   ngOnChanges(): void {
    this.countries = this.covidData?.Countries;
+  }
+  
+  // To prevent memory leak 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
