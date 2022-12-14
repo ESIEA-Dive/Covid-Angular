86911c7492f3f57a9dbae7b522d25026553cc3bb
unknown <dive@et.esiea.fr>
2022-12-14 22:24:02 +0100

    Shared Search in Summary

---
+++ covid-tracker/src/app/summary/components/confirmed-bycountry/confirmed-bycountry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmed-bycountry',
+  templateUrl: './confirmed-bycountry.component.html',
+  styleUrls: ['./confirmed-bycountry.component.css']
+})
+export class ConfirmedBycountryComponent {
+
+}
