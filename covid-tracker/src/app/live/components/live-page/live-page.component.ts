7b103805b59f5cb0fd16d35ef18f2df8bbd46b8e
unknown <dive@et.esiea.fr>
2022-12-15 17:23:48 +0100

    High Charts Options - live-page.component.ts

--- covid-tracker/src/app/live/components/live-page/live-page.component.ts
+++ covid-tracker/src/app/live/components/live-page/live-page.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as Hightcharts from 'highcharts/highmaps';
+import * as worldMap from '@highcharts/map-collection/custom/world.geo.json';
 
 @Component({
   selector: 'app-live-page',
   templateUrl: './live-page.component.html',
   styleUrls: ['./live-page.component.css']
 })
-export class LivePageComponent {
+export class LivePageComponent implements OnInit {
+
+  constructor() { }
+
+  Highcharts: typeof Hightcharts = Hightcharts;
+  chartConstructor = 'mapChart';
+
+  //options
+  chartOptions: Hightcharts.Options | any = {
+    chart: {
+      map: worldMap
+    },
+    title: {
+      text: 'COVID-19 World Data'
+    },
+    subtitle: {
+      text: 'Confirmed Cases Live'
+    },
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        alignTo: 'spacingBox'
+      }
+    },
+    legend: {
+      enabled: true
+    },
+    colorAxis: {
+      min:0
+    },
+    series: [{
+      type: 'map',
+      name: 'COVID-19 Data',
+      state: {
+        hover: {
+          color: '#DC3545'
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        format: '{point.name}'
+      },
+      allAreas: false,
+      data: [
+        ["do", 0],
+        ['um',1],
+        ['us',2]
+      ]
+    }]
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
